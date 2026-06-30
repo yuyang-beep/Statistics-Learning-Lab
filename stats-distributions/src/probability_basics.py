4a10efa65e7a36b592f680ec99c3a838ab6f3ea9
yuyang-beep <yywu0512@gmail.com>
2026-06-30 22:19:05 +0800

    Learning objective: Kolmogorov axioms & classical probability Implementation: SampleSpace class with P, union, intersect, complement, inclusion_exclusion Reflection: Clarified mutual exclusivity vs independence; first Python class written from scratch

---
+++ stats-distributions/src/probability_basics.py
@@ -0,0 +1,26 @@
+class SampleSpace:
+    def __init__(self, outcomes):
+        self.omega = set(outcomes)
+
+    def P(self, event):
+        return len(event) / len(self.omega)
+
+    def union(self, A, B):
+        return A | B
+
+    def intersect(self, A, B):
+        return A & B
+
+    def complement(self, A):
+        return self.omega - A
+    
+    def inclusion_exclusion(self, A, B):
+        return self.P(A) + self.P(B) - self.P(self.intersect(A, B))
+
+cards = SampleSpace(range(1, 14))
+A = {1, 2, 3}
+B = {1, 11, 12, 13}
+
+print(cards.P(A))
+print(cards.P(B))
+print(cards.inclusion_exclusion(A, B))
